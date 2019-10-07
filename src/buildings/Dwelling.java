488a8b713e8e7bc954d2aa41877ab46e1923eeec
DDKosh <d.coshelewa@gmail.com>
2019-10-07 21:32:37 +0400

    да я это и то что и всё

--- src/buildings/Dwelling.java
+++ src/buildings/Dwelling.java
@@ -4,93 +4,120 @@ public class Dwelling {
 
     private DwellingFloor[] house;
 
-    public Dwelling(int numbOfFloors, int[] numbOfFlats){
+    public Dwelling(int numbOfFloors, int[] numbOfFlats) {
         house = new DwellingFloor[numbOfFloors];
-        for (int i = 0; i < numbOfFloors; i++){
+        for (int i = 0; i < numbOfFloors; i++) {
             house[i] = new DwellingFloor(numbOfFlats[i]);
         }
     }
 
-    public Dwelling(DwellingFloor[] floors){
+    public Dwelling(DwellingFloor[] floors) {
         this.house = floors;
     }
 
-    public int getFloors(){
+    public int getFloors() {
         return house.length;
     }
 
-    public int getFlats(){
-        int flats = 0;
-        for (DwellingFloor floor: house) {
-            flats += floor.quantityOfFlats();
+    public int getFlatsQuantity() {
+        int flatsQuantity = 0;
+        for (DwellingFloor floor : house) {
+            flatsQuantity += floor.quantityOfFlats();
         }
-        return flats;
+        return flatsQuantity;
     }
 
-    public int getArea(){
+    public int getArea() {
         int area = 0;
-        for (DwellingFloor floor: house) {
+        for (DwellingFloor floor : house) {
             area += floor.getArea();
         }
         return area;
     }
 
-    public int getRoom(){
+    public int getRoom() {
         int rooms = 0;
-        for (DwellingFloor floor: house) {
+        for (DwellingFloor floor : house) {
             rooms += floor.getRooms();
         }
         return rooms;
     }
 
-    public DwellingFloor[] getArrayFloors(){
+    public DwellingFloor[] getArrayFloors() {
         return house;
     }
 
-    public DwellingFloor getFloor(int index){
+    public DwellingFloor getFloor(int index) {
         return house[index];
     }
 
-    public void setFloor(int index, DwellingFloor floor){
+    public void setFloor(int index, DwellingFloor floor) {
         house[index] = floor;
     }
-    
-    public Flat getFlat(int index){
+
+    public Flat getFlat(int index) {
         int indexFlat = 0;
-        for (DwellingFloor floor:house) {
-            for (Flat flat: floor.getFlats())
-            {
-                if(index == indexFlat) return flat;
+        for (DwellingFloor floor : house) {
+            for (Flat flat : floor.getFlats()) {
+                if (index == indexFlat) return flat;
                 indexFlat++;
             }
         }
         return null;
     }
 
-    public void replaceFlat(int index, Flat flat){
+    public void replaceFlat(int index, Flat flat) {
         Flat replaceableFlat = getFlat(index);
         replaceableFlat = flat;
     }
 
 
-    public void addFlat(int index, Flat newFlat){
+    public void addFlat(int index, Flat newFlat) {
+        DwellingFloor desiredFloor = house[0];
+        int desiredIndex = 0;
+        findFlat(index, desiredIndex, desiredFloor);
+        desiredFloor.addFlat(index - desiredIndex - 1, newFlat);
+    }
 
-        if(index + 1 >= flats.length){
-            Flat[] copyFlats = new Flat[flats.length*2];
-            System.arraycopy(this.flats,0,copyFlats,0,flats.length);
+    public void removeFlat(int index) {
+        DwellingFloor desiredFloor = house[0];
+        int desiredIndex = 0;
+        findFlat(index, desiredIndex, desiredFloor);
+        desiredFloor.removeFlat(index - desiredIndex - 1);
+    }
+
+    private void findFlat(int index, int desiredIndex, DwellingFloor desiredFloor) {
+
+        int numOfFlat = 0;
+        loop:
+        for (DwellingFloor floor : house) {
+            desiredIndex = floor.quantityOfFlats() - 1;
+            for (Flat flat : floor.getFlats()) {
+                if (numOfFlat == index) {
+                    desiredFloor = floor;
+                    break loop;
+                }
+                numOfFlat++;
+            }
         }
-        System.arraycopy(this.flats,index, flats,index+1,flats.length-index);
     }
 
-    public void removeFlat(int index){
-        System.arraycopy(flats,index+1,flats,index,flats.length - index);
-    }
-
-    public int getBestSpace(){
+    public int getBestSpace() {
         int bestSpace = 0;
-        for (Flat flat:flats) {
-            if(flat.getArea() > bestSpace) bestSpace = flat.getArea();
+        for (DwellingFloor floor : house) {
+            for (Flat flat : floor.getFlats()) {
+                if (flat.getArea() > bestSpace) bestSpace = flat.getArea();
+            }
         }
         return bestSpace;
+    }
+
+    public Flat[] sortedFlats(){
+        Flat[] flats = new Flat[getFlatsQuantity()];
+        for (int i = flats.length - 1; i > 0; i--)
+            for (int j = 0; j < i; j++){
+                if(fl)
+            }
+        return flats;
     }
 }
--- src/buildings/DwellingFloor.java
+++ src/buildings/DwellingFloor.java
@@ -1,5 +1,5 @@
 package buildings;
-
+ 
 public class DwellingFloor {
     private Flat[] flats;
 
